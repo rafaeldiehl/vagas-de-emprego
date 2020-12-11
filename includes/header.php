2ec56ad3bd88b066c2486d33626dfad4afe7a46f
Rafael <rafaeljuliani1984@gmail.com>
2020-12-11 02:36:14 -0300

    feat: Add starting page

---
+++ includes/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Vagas de Emprego</title>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="assets/styles/normalize.css" />
+  <link rel="stylesheet" href="assets/styles/main.css" />
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+  require_once './vendor/autoload.php';
+
+  include_once "./includes/header.php";
+  include_once "./includes/pages/home.php";
+  include_once "./includes/footer.php";
